75e225c6fb06f7d91712735ce13bdf3e0c77be72
GODOFL <1041588869@qq.com>
2024-05-23 12:43:56 +0800

    Add Box

--- PrimalEditor/Utilities/Controls/VectorBox.cs
+++ PrimalEditor/Utilities/Controls/VectorBox.cs
@@ -8,7 +8,31 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace PrimalEditor.Utilities.Controls {
-     class VectorBox : Control {
+
+    public enum VectorType { 
+        Vector2,
+        Vector3, 
+        Vector4,
+    }
+
+    class VectorBox : Control {
+        public VectorType VectorType {
+            get => (VectorType)GetValue(VectorTypeProperty);
+            set => SetValue(VectorTypeProperty, value);
+        }
+
+        public static readonly DependencyProperty VectorTypeProperty =
+            DependencyProperty.Register(nameof(VectorType), typeof(VectorType), typeof(VectorBox),
+                new PropertyMetadata(VectorType.Vector3));
+
+        public Orientation Orientation {
+            get => (Orientation)GetValue(OrientationProperty);
+            set => SetValue(OrientationProperty, value);
+        }
+
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(VectorBox),
+                new PropertyMetadata(Orientation.Horizontal));
 
         public double Multiplier {
             get => (double)GetValue(MultiplierProperty);
@@ -16,7 +40,7 @@ namespace PrimalEditor.Utilities.Controls {
         }
 
         public static readonly DependencyProperty MultiplierProperty =
-            DependencyProperty.Register(nameof(Multiplier), typeof(double), typeof(NumberBox),
+            DependencyProperty.Register(nameof(Multiplier), typeof(double), typeof(VectorBox),
                 new PropertyMetadata(1.0));
 
         // 向量X，Y，Z，W的依赖属性
@@ -26,7 +50,7 @@ namespace PrimalEditor.Utilities.Controls {
         }
 
         public static readonly DependencyProperty XProperty =
-            DependencyProperty.Register(nameof(X), typeof(string), typeof(NumberBox),
+            DependencyProperty.Register(nameof(X), typeof(string), typeof(VectorBox),
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         public string Y {
@@ -35,7 +59,7 @@ namespace PrimalEditor.Utilities.Controls {
         }
 
         public static readonly DependencyProperty YProperty =
-            DependencyProperty.Register(nameof(Y), typeof(string), typeof(NumberBox),
+            DependencyProperty.Register(nameof(Y), typeof(string), typeof(VectorBox),
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         public string Z {
@@ -44,7 +68,7 @@ namespace PrimalEditor.Utilities.Controls {
         }
 
         public static readonly DependencyProperty ZProperty =
-            DependencyProperty.Register(nameof(Z), typeof(string), typeof(NumberBox),
+            DependencyProperty.Register(nameof(Z), typeof(string), typeof(VectorBox),
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         public string W {
@@ -53,7 +77,7 @@ namespace PrimalEditor.Utilities.Controls {
         }
 
         public static readonly DependencyProperty WProperty =
-            DependencyProperty.Register(nameof(W), typeof(string), typeof(NumberBox),
+            DependencyProperty.Register(nameof(W), typeof(string), typeof(VectorBox),
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         static VectorBox() {
